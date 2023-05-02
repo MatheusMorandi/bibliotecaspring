b000d1c6451f9f4d0fc8dff27217eaff8d6590b1
MatheusMorandi <theu.morandi@gmail.com>
2023-05-02 19:49:32 +0000

    Att

--- app/src/main/java/application/controller/LivroController.java
+++ app/src/main/java/application/controller/LivroController.java
@@ -1,13 +1,25 @@
 package application.controller;
 
+import application.model.LivroRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LivroController {
 
+    @Autowired
+    private LivroRepository livroRepo;
+
     @RequestMapping("/livro")
-    public String list(){
+    public String list(Model model){
+
+        model.addAttribute("livros", livroRepo.findAll());
 
         return "WEB-INF/list.jsp";
 
--- app/src/main/webapp/WEB-INF/list.jsp
+++ app/src/main/webapp/WEB-INF/list.jsp
@@ -1,4 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib uri="jakarta.tags.core" prefix="c" %>
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -14,6 +15,12 @@
     <body>
 
         <h1>Livros</h1>
+
+        <c:forEach var = "item" items = "${livros}">
+
+            ${item.titulo} <br/>
+
+        </c:forEach>
 
     </body>
 
